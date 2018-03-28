67e3864c2912b46c9f7f627b56ae1bbbf06c808f
CosasDePuma <kikefontanlorenzo@gmail.com>
2018-03-28 18:51:09 +0200

    Alertas en el login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController } from 'ionic-angular';
 
 import { RestProvider } from '../../providers/rest/rest';
 
@@ -12,16 +12,45 @@ export class HomePage {
   
   data: any;
 
-  constructor(public navCtrl: NavController, public rest: RestProvider) {
+  user:string;
+  pass:string;
+
+  constructor(public navCtrl: NavController, private rest: RestProvider, private alertCtrl: AlertController) {
+    this.getData();
   }
 
-  login() {
-
+  getData() {
     this.rest.getGalicienciaData().then(
       (res) => {
         this.data = JSON.parse(JSON.stringify(res)).data;
         console.log(this.data);
       }
     );
+  }
+
+  checkLogin() {
+    if(this.user == undefined || this.pass == undefined || this.user == '' || this.pass == '') {
+      this.emptyCredentials();
+    } else {
+      console.log("Comprobación de datos");
+    }
+  }
+
+  emptyCredentials() {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: 'Debe rellenar todos los campos.',
+      buttons: ['Aceptar']
+    });
+    alert.present();
+  }
+
+  wrongCredentials() {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: 'Las credenciales de acceso no se corresponden a ningún usuario.',
+      buttons: ['Aceptar']
+    });
+    alert.present();
   }
 }
