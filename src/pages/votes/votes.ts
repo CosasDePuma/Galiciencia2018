18b786ead377ec22012240580ce21996b387eebc
CosasDePuma <kikefontanlorenzo@gmail.com>
2018-04-04 01:58:16 +0200

    Funcionan las votaciones!

--- src/pages/votes/votes.ts
+++ src/pages/votes/votes.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController, IonicModule } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, AlertController, ToastController } from 'ionic-angular';
 
 import { RestProvider } from '../../providers/rest/rest';
 
@@ -20,6 +20,7 @@ export class VotesPage {
   
   MAX_GROUPS = 5
   group: number = undefined
+  group_member: number = undefined
 
   custom_projects = []
   all_projects: any[] = undefined
@@ -27,12 +28,21 @@ export class VotesPage {
   current_dropdown: string = undefined
   dropdown_menu: Array<{ id: number, title: string }> = []
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, private rest: RestProvider) {   
-    this.group = this.navParams.get('group');
+  puntuacion = [ 1, 1, 1, 1, 1 ]
+
+  constructor(
+    private rest: RestProvider,
+    public navParams: NavParams,
+    public navCtrl: NavController,
+    public modalCtrl: ModalController,
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController
+  ) {   
+    this.group = this.navParams.get('group')
+    this.group_member = this.navParams.get('group_member')
   }
 
   ionViewDidLoad() {
-    console.log("Grupo: " + this.group)
     this.getProyectos()
   }
 
@@ -58,10 +68,73 @@ export class VotesPage {
     for (let i = 0; i < this.custom_projects.length; i++) {
       this.dropdown_menu.push({ id: this.custom_projects[i].id, title: this.custom_projects[i].title })
     }
-    console.log("Done")
   }
 
-  projectSelected() {
-    console.log(this.current_dropdown)
+  sendPuntuacion() {
+    if(this.current_dropdown === undefined) {
+      this.noProjectSelected()
+    } else {
+      this.confirmarVotacion(this.calcularMedia())
+    }
+  }
+
+  noProjectSelected() {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: 'Debe seleccionar un proyecto.',
+      buttons: ['Aceptar']
+    })
+    alert.present()
+  }
+
+  calcularMedia() {
+    let media = 0
+    for (let i = 0; i < this.puntuacion.length; i++) {
+      media += this.puntuacion[i]
+    }
+    return (media / this.puntuacion.length)
+  }
+
+  confirmarVotacion(media: number) {
+    let alert = this.alertCtrl.create({
+      title: 'Confirmar votación',
+      message: '¿Seguro que quieres puntuar al proyecto número '
+        + this.current_dropdown + ' con una media de ' + media + '?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Enviar',
+          handler: () => {
+            this.enviarInfo(media)
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  enviarInfo(media: number) {
+    this.rest.updateProyectos(this.current_dropdown, this.group_member, media).then(
+      (res) => {
+          this.navCtrl.setRoot(this.navCtrl.getActive().component, {
+            group: this.group,
+            group_member: this.group_member
+          });
+          
+          let toast = this.toastCtrl.create({
+            message: 'Votación enviada correctamente',
+            duration: 2000,
+            position: 'bottom'
+          })
+
+          toast.present()
+
+      }
+    )
   }
 }
